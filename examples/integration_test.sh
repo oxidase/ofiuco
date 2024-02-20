7251f068933beccd3c822e090e882f36b391c570
Michael Krasnyk <michael.krasnyk@gmail.com>
2024-02-20 14:44:20 +0700

    chore: drop :distribution targets, fix #32

--- examples/integration_test.sh
+++ examples/integration_test.sh
@@ -1,2 +1,4 @@
+#!/bin/sh
+BUILD_WORKSPACE_DIRECTORY=$(readlink -f MODULE.bazel)/..
 cd examples/$1
-bazel test ...  --test_output=errors --spawn_strategy=local
+bazel test ... --test_output=errors --spawn_strategy=local --override_module=rules_poetry=$BUILD_WORKSPACE_DIRECTORY
