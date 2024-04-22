27328444372dba2fe3a94fed6b3da4065d9354e6
Michael Krasnyk <michael.krasnyk@gmail.com>
2024-04-22 16:13:22 +0200

    fix: allow to use py_venv with Python < 3.12

--- python/py_venv.py
+++ python/py_venv.py
@@ -1,27 +1,29 @@
 import argparse
 import filecmp
+import os
 import warnings
 from pathlib import Path
 
 SKIP_SET = {Path("requirements.txt")}
 
-if __name__ == "__main__":
+
+def main(argv=None):
     parser = argparse.ArgumentParser(description="Download and install a Poetry package")
 
     parser.add_argument("target", type=Path, help="output virtual environment directory")
     parser.add_argument("path", type=Path, nargs="*", help="python package path")
 
-    args = parser.parse_args()
+    args = parser.parse_args(argv)
 
     for python_path in args.path:
         if not python_path.exists() or not python_path.is_dir():
             raise RuntimeError(f"Required Python package directory {python_path} does not exist")
 
-        for directory_path, _, file_names in python_path.walk():
-            in_package_directory = directory_path.relative_to(python_path)
+        for directory_path, _, file_names in os.walk(python_path):
+            in_package_directory = Path(os.path.relpath(directory_path, python_path))
             target_directory = args.target / in_package_directory
             target_directory.mkdir(parents=True, exist_ok=True)
-            relative_directory = directory_path.relative_to(target_directory, walk_up=True)
+            relative_directory = Path(os.path.relpath(directory_path, target_directory))
 
             for file_name in file_names:
                 if in_package_directory / file_name in SKIP_SET:
@@ -30,11 +32,15 @@ if __name__ == "__main__":
                 symlink_path = target_directory / file_name
                 target_path = relative_directory / file_name
                 if symlink_path.exists():
-                    if not filecmp.cmp(symlink_path, directory_path / file_name, shallow=False):
+                    if not filecmp.cmp(symlink_path, Path(directory_path) / file_name, shallow=False):
                         warnings.warn(
-                            f"{symlink_path} already exists and points to {symlink_path.readlink()}\n"
+                            f"{symlink_path} already exists and points to {os.path.realpath(symlink_path)}\n"
                             + f"Skip {target_path} which seems to have different contents"
                         )
                     continue
 
                 symlink_path.symlink_to(target_path)
+
+
+if __name__ == "__main__":
+    main()
---
+++ python/tests/py_venv_test.py
@@ -0,0 +1,45 @@
+import glob
+import os
+import tempfile
+import unittest
+import zipfile
+
+from python.py_venv import SKIP_SET, main
+
+WHEEL_FILE = "python/tests/resources/six-1.16.0-py2.py3-none-any.whl"
+
+
+class TestInstallSubcommand(unittest.TestCase):
+    def test_main(self):
+        tmpdir = os.environ.get("TEST_TMPDIR", tempfile.gettempdir())
+        target = os.path.join(tmpdir, "venv")
+        pkg_paths = [os.path.join(tmpdir, "pkg1"), os.path.join(tmpdir, "pkg2")]
+
+        for path in pkg_paths:
+            with zipfile.ZipFile(WHEEL_FILE, "r") as zip_ref:
+                zip_ref.extractall(path)
+
+        for index, name in enumerate(pkg_paths):
+            with open(os.path.join(name, "duplicated_name"), "wt") as f:
+                f.write(str(index))
+
+        test_package = os.path.join(tmpdir, "test_pkg")
+        os.makedirs(os.path.join(test_package, "test"))
+        for name in list(SKIP_SET) + ["test_file", "test/test_file"]:
+            with open(os.path.join(test_package, name), "wt") as f:
+                f.write("test")
+
+        with self.assertWarnsRegex(UserWarning, expected_regex="Skip"):
+            main([target, test_package] + pkg_paths)
+
+        venv_resolved_files = glob.glob(os.path.join(target, "**"), recursive=True)
+        venv_files = [os.path.relpath(name, target) for name in venv_resolved_files]
+
+        assert all(os.path.islink(name) or os.path.isdir(name) for name in venv_resolved_files)
+        assert "test_file" in venv_files
+        assert "test/test_file" in venv_files
+        assert "requirements.txt" not in venv_files
+
+
+if __name__ == "__main__":
+    unittest.main()
