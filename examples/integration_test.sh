25154b96fe237654ae3e97a50ce42715f12b6917
Michael Krasnyk <michael.krasnyk@gmail.com>
2024-06-07 10:55:42 +0200

    feat: use bazel versions 6.x..last_green for testing

--- examples/integration_test.sh
+++ examples/integration_test.sh
@@ -3,11 +3,14 @@ BUILD_WORKSPACE_DIRECTORY=$(dirname $(readlink -f WORKSPACE))
 if [ ! -z ${TEST_TMPDIR+x} -a $1 != "transitions" ]; then
   export TMPDIR=$TEST_TMPDIR
 fi
+export HOME=$TEST_TMPDIR
 
 cd examples/$1
-bazel test ... \
-      --test_output=errors \
-      --spawn_strategy=local\
-      --verbose_failures \
-      --override_module=rules_poetry=$BUILD_WORKSPACE_DIRECTORY \
-      --override_repository=rules_poetry=$BUILD_WORKSPACE_DIRECTORY
+
+ARGS="--test_output=errors --spawn_strategy=local --verbose_failures"
+ARGS="$ARGS --override_repository=rules_poetry=$BUILD_WORKSPACE_DIRECTORY"
+[ -f MODULE.bazel ] && ARGS="$ARGS --override_module=rules_poetry=$BUILD_WORKSPACE_DIRECTORY"
+
+echo "Using $(bazelisk version)"
+bazelisk test ... $ARGS
+
