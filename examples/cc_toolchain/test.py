527f334c1183a98e097556ecff24af6bf35de89e
Michael Krasnyk <michael.krasnyk@gmail.com>
2024-08-06 23:36:29 +0200

    feat: add evdev test to linux

--- examples/cc_toolchain/test.py
+++ examples/cc_toolchain/test.py
@@ -3,6 +3,7 @@ import tomllib
 
 import pytest
 
+import evdev
 import llama_cpp
 from llama_cpp.llama_cpp import _lib as llama_lib
 from llama_cpp.llama_cpp import _lib_base_name as lib_base_name
@@ -23,6 +24,14 @@ def test_llama_library_rpath():
     assert llama_lib._name == lib._name
     with open(lib._name, "rb") as handle:
         assert b"bazel-out/" in handle.read()
+
+
+def test_evdev():
+    devices = [evdev.InputDevice(path) for path in evdev.list_devices()]
+    for device in devices:
+        print(device.path, device.name, device.phys)
+
+    assert len(evdev.list_devices()) > 0
 
 
 if __name__ == "__main__":
