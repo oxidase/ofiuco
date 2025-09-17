3f80f7319637ec1c34088bcf2e02cfee7b4ea1ae
Michael Krasnyk <michael.krasnyk@gmail.com>
2025-09-17 12:13:30 +0200

    fix: allow to build on MacOS 26.0

--- examples/uv/test.py
+++ examples/uv/test.py
@@ -1,16 +1,35 @@
 import sys
 import tomllib
 
-import boto3
 import pytest
 
 
 def test_boto3_version():
+    import boto3
+
     with open("uv.lock", "rb") as uv_lock:
         dependencies = tomllib.load(uv_lock)
     locked_boto3 = next((package for package in dependencies["package"] if package["name"] == "boto3"), None)
     assert locked_boto3
     assert locked_boto3["version"] == boto3.__version__
+
+
+def test_all_imports():
+    import pip
+
+    assert pip
+
+    import pre_commit
+
+    assert pre_commit
+
+    import setuptools
+
+    assert setuptools
+
+    import sphinxcontrib.images as images
+
+    assert images
 
 
 if __name__ == "__main__":
